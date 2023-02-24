bdac0659c919b9170bda750cf9bda53a26ffd425
Bhawan sharma <bhawansharma15@gmail.com>
2023-02-24 16:10:19 +0530

    commit

---
+++ javascript.js
@@ -0,0 +1,7 @@
+document.querySelector(".burger").addEventListener("click", function () {
+  document.querySelector(".mobile-box").style.display = "block";
+});
+
+document.querySelector(".burger-cross").addEventListener("click", function () {
+  document.querySelector(".mobile-box").style.display = "none";
+});
